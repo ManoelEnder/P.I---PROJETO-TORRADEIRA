ff9809f88b1e3ad3669d9d1491d26097a24e12ae
Jullya2 <jullya.monteiro@alunos.sc.senac.br>
2026-04-02 18:18:48 -0300

    Reapply "Merge branch 'main' of https://github.com/ManoelEnder/P.I---PROJETO-TORRADEIRA"
    This reverts commit d3a6ae6fee2f6ae341368fd052e3464870be3649.

--- PROJETO TORRADEIRA/Assets/Scripts/Camera/RetroTVEffect.cs
+++ PROJETO TORRADEIRA/Assets/Scripts/Camera/RetroTVEffect.cs
@@ -3,31 +3,40 @@ using UnityEngine.UI;
 
 public class RetroTVEffect : MonoBehaviour
 {
-    public float speed = 50f;
-    public float flickerSpeed = 10f;
-    public float flickerAmount = 0.1f;
+    public RectTransform noise;
+    public RectTransform scanlines;
 
-    RectTransform rect;
-    Image img;
-    Color baseColor;
+    public float noiseSpeed = 50f;
+    public float scanSpeed = 20f;
+
+    public Image noiseImg;
+    public Image scanImg;
+
+    float baseNoiseAlpha;
+    float baseScanAlpha;
 
     void Start()
     {
-        rect = GetComponent<RectTransform>();
-        img = GetComponent<Image>();
-        baseColor = img.color;
+        baseNoiseAlpha = noiseImg.color.a;
+        baseScanAlpha = scanImg.color.a;
     }
 
     void Update()
     {
-        rect.anchoredPosition += Vector2.up * speed * Time.deltaTime;
+    
+        noise.anchoredPosition += new Vector2(30f, noiseSpeed) * Time.deltaTime;
 
-        if (rect.anchoredPosition.y > Screen.height)
-        {
-            rect.anchoredPosition = new Vector2(0, -Screen.height);
-        }
+        scanlines.anchoredPosition += Vector2.up * scanSpeed * Time.deltaTime;
 
-        float flicker = 1 + Mathf.Sin(Time.time * flickerSpeed) * flickerAmount;
-        img.color = new Color(baseColor.r, baseColor.g, baseColor.b, baseColor.a * flicker);
+        if (noise.anchoredPosition.y > Screen.height)
+            noise.anchoredPosition = new Vector2(0, -Screen.height);
+
+        if (scanlines.anchoredPosition.y > Screen.height)
+            scanlines.anchoredPosition = new Vector2(0, 0);
+
+        float flicker = 1 + Mathf.Sin(Time.time * 12f) * 0.05f;
+
+        noiseImg.color = new Color(1, 1, 1, baseNoiseAlpha * flicker);
+        scanImg.color = new Color(1, 1, 1, baseScanAlpha * flicker);
     }
 }
