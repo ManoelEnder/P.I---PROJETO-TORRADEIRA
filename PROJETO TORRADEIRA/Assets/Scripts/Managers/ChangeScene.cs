e31764c32eae16ac86ce7371d9a5b5fd1a8884df
Manoel Scheidt Ender <manoel.ender@alunos.sc.senac.br>
2026-08-25 08:20:50 -0300

    Arrumei o botão de voltar

--- PROJETO TORRADEIRA/Assets/Scripts/Managers/ChangeScene.cs
+++ PROJETO TORRADEIRA/Assets/Scripts/Managers/ChangeScene.cs
@@ -5,6 +5,8 @@ public class ChangeScene : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
+        Debug.Log("Botão clicado! Cena: " + sceneName);
+
         SceneManager.LoadScene(sceneName);
     }
 }
